6b6912cf5982ac7581760a5492c1c649c4106035
Azat <azat24680@gmail.com>
2025-05-07 15:48:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function checkPassword() {
     const input = document.getElementById('password').value;
-    const key = atob("dG0tMTk5MQ==");
+    const key = atob("a2V5MjAyNQ==");
+
+    if (input === key) {
+        document.getElementById('links').style.display = 'block';
+    } else {
+        alert('Неверный пароль');
+    }
+}
+
+function checkPasswordKey() {
+    const input = document.getElementById('password').value;
+    const key = atob("V0R4MTYxNzMwLDE3NDY2MDgwOGd4bC5MZ2Fzb3I=");
 
     if (input === key) {
         document.getElementById('links').style.display = 'block';
